eaaaafc69ba95b16077f5099b7ee4a32587f2c59
?ngel ?lvarez Serra <angel@nan-tic.com>
2013-11-13 16:51:40 +0100

    initial commit

---
+++ __init__.py
@@ -0,0 +1,11 @@
+#The COPYRIGHT file at the top level of this repository contains the full
+#copyright notices and license terms.
+
+from trytond.pool import Pool
+from .production import *
+
+
+def register():
+    Pool.register(
+        Production,
+        module='production_origin', type_='model')
